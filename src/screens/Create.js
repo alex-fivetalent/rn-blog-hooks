c79976f72ddb6f996dc73ee32e697052d234998d
Alex Hanson <alex.hanson@fivetalent.com>
2020-03-25 08:39:06 -0700

    init commit

---
+++ src/screens/Create.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {Block}
+
+const CreateScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default CreateScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/Index.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react'
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native'
+import BlogContext from '../context/BlogContext'
+
+const IndexScreen = () => {
+    const {data, addBlogPost} = useContext(BlogContext)
+    return (
+        <View>
+            <Text>INDEXING</Text>
+            <Button title="Add Post" onPress={addBlogPost}/>
+            <FlatList data={data} keyExtractor={(blogPost) => blogPost.title} renderItem={({item}) => {return (<Text>{item.title}</Text>)}}/>
+        </View>
+    )
+}
+
+export default IndexScreen
+
+const styles = StyleSheet.create({})
